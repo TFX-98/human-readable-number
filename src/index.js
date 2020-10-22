f620f85045c220a9057fb650cf129bb86e0d0801
TFX-98 <66826440+TFX-98@users.noreply.github.com>
2020-10-22 22:52:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let obj = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety',
+    }
+
+    let numberArr = number.toString().split('');
+    if (number >= 0 && number <= 19) {
+        return obj[number];
+    } else if (number <= 99 && number % 10 == 0) {
+        return obj[number];
+    } else if (number <= 99) {
+        return obj[+numberArr[0] * 10] + ' ' + obj[+numberArr[1]];
+    } else if (number % 100 == 0) {
+        return obj[number / 100] + ' hundred';
+    } else if (numberArr[1] == 0) {
+        return obj[+numberArr[0]] + ' hundred ' + obj[+numberArr[2]];
+    } else if (+(numberArr[1] + numberArr[2]) <= 19) {
+        return obj[+numberArr[0]] + ' hundred ' + obj[+(numberArr[1] + numberArr[2])];
+    } else if (numberArr[2] == 0) {
+        return obj[+numberArr[0]] + ' hundred ' + obj[+numberArr[1] * 10];
+    } else {
+        return obj[+numberArr[0]] + ' hundred ' + obj[+numberArr[1] * 10] + ' ' + obj[+numberArr[2]];
+    }
+
 }
